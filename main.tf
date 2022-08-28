6b38f9c6e77e57e3fc76d05fc06ffd8de899a482
Tom Goodsell <ymylei@users.noreply.github.com>
2022-08-28 01:10:03 -0500

    init test SAML app

---
+++ main.tf
@@ -0,0 +1,14 @@
+resource "okta_app_saml" "test" {
+  label                    = "test1"
+  sso_url                  = "https://example.com"
+  recipient                = "https://example.com"
+  destination              = "https://example.com"
+  audience                 = "https://example.com/audience"
+  subject_name_id_template = "$${user.userName}"
+  subject_name_id_format   = "urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress"
+  response_signed          = true
+  signature_algorithm      = "RSA_SHA256"
+  digest_algorithm         = "SHA256"
+  honor_force_authn        = false
+  authn_context_class_ref  = "urn:oasis:names:tc:SAML:2.0:ac:classes:PasswordProtectedTransport"
+}
---
+++ terraform.tf
@@ -0,0 +1,7 @@
+terraform {
+  required_providers {
+      okta = {
+          source = "okta/okta"
+      }
+  }
+}
